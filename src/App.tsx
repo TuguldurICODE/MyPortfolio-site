17f47066bfaf41b1942a97accaf4cfa1cb53804c
Tuguldur Erendoo <toogii.erendoo@gmail.com>
2025-11-11 22:47:56 +0800

    portfolio day 1

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <>
       <h1 className="text-3xl font-bold text-gray-500 text-center">
-        Hello world!
+        Haildai juu jamdaaa dunduuuuuu haildai jaaa yu
       </h1>
     </>
   )
---
+++ src/firebase/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC-dHcaIDfme_5xzVlSzmTWRbDJH5X5V-o",
+  authDomain: "myportfolio-tuguldur.firebaseapp.com",
+  projectId: "myportfolio-tuguldur",
+  storageBucket: "myportfolio-tuguldur.firebasestorage.app",
+  messagingSenderId: "722308809815",
+  appId: "1:722308809815:web:455b5bd914c6ed3467cf12"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
