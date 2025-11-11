b8a932cf004e448d776bf70915505a884236891e
Tuguldur Erendoo <toogii.erendoo@gmail.com>
2025-11-11 21:42:55 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold text-gray-500 text-center">
+        Hello world!
+      </h1>
+    </>
+  )
+}
+
+export default App
